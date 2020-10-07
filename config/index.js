0afd8c5fa7de0264d81f4a2740e9a5bfc2e1575c
deepak.g <deepak.g@krds.fr>
2020-10-07 19:54:42 +0530

    config added

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 let config = {
 }
 // mongodb - connection string 
-config.mongodbUrl = '';
+config.mongodbUrl = 'mongodb://democosmosdbgmk:yAZpH8VF9FiG2y5e6fJGzbCWtnujD6sziSJf2UVWv3n1BlGn8SNQPFU54UJm5sxPiqzF4qJ6hZRwMrhrA5gE1Q==@democosmosdbgmk.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@democosmosdbgmk@';
 
 config.timezone = 'Asia/Kolkata'
 
