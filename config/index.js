7887010f27568524caf7b5210daafcb6475a04bc
deepak.g <deepak.g@krds.fr>
2020-10-08 15:29:16 +0530

    chnage config

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 let config = {
 }
 // mongodb - connection string 
-config.mongodbUrl = '';
+config.mongodbUrl = 'mongodb://dbdemogmk:1oTNpUkDrREtHi2CYQpFeURFxq3Lj8ZrcJkrq65A42q8MPH6qzY3xt5TbdMEC8dMsx9OpKbBUjUA1E2kPNmrkg==@dbdemogmk.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@dbdemogmk@';
 
 config.timezone = 'Asia/Kolkata'
 
